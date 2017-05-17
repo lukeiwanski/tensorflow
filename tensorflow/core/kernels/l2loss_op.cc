f97a5f97bcabcb0929f04ef95fa234f247675e24
biessy_r <romain.biessy@codeplay.com>
2017-05-17 12:12:09 +0100

    [OpenCL] Registers op l2loss_op.cc

--- tensorflow/core/kernels/l2loss_op.cc
+++ tensorflow/core/kernels/l2loss_op.cc
@@ -28,6 +28,9 @@ namespace tensorflow {
 
 typedef Eigen::ThreadPoolDevice CPUDevice;
 typedef Eigen::GpuDevice GPUDevice;
+#ifdef TENSORFLOW_USE_SYCL
+typedef Eigen::SyclDevice SYCLDevice;
+#endif  // TENSORFLOW_USE_SYCL
 
 template <typename Device, typename T>
 class L2LossOp : public OpKernel {
@@ -85,5 +88,15 @@ REGISTER_GPU_KERNEL(Eigen::half);
 #undef REGISTER_GPU_KERNEL
 
 #endif  // GOOGLE_CUDA
+
+#ifdef TENSORFLOW_USE_SYCL
+#define REGISTER_SYCL_KERNEL(T)                                  \
+  REGISTER_KERNEL_BUILDER(                                       \
+      Name("L2Loss").Device(DEVICE_SYCL).TypeConstraint<T>("T"), \
+      L2LossOp<SYCLDevice, T>);
+
+TF_CALL_GPU_NUMBER_TYPES_NO_HALF(REGISTER_SYCL_KERNEL);
+#undef REGISTER_SYCL_KERNEL
+#endif  // TENSORFLOW_USE_SYCL
 
 }  // namespace tensorflow
