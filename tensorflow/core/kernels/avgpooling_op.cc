dc371e39130e31eb25aadcab3e1217b22a9c494d
Jeffrey <jeffrey.mak@codeplay.com>
2017-06-28 09:37:35 +0100

    Removing whitespace

--- tensorflow/core/kernels/avgpooling_op.cc
+++ tensorflow/core/kernels/avgpooling_op.cc
@@ -21,7 +21,6 @@ limitations under the License.
 
 #include <vector>
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
-
 #include "tensorflow/core/framework/numeric_op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/register_types.h"
